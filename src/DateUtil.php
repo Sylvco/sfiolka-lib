b7c8e654931635b8d9a196007860c44606b541a1
sfiolka <sylwester.fiolka@gmail.com>
2021-07-25 11:36:16 +0200

    extend DateUtil

--- src/DateUtil.php
+++ src/DateUtil.php
@@ -7,6 +7,16 @@ use DateTime;
 class DateUtil
 {
 
+    public static function nowFilenameFormat(): string
+    {
+        return self::format(new DateTime(), 'Y-m-d-H-i-s');
+    }
+
+    public static function nowMysqlFormat(): string
+    {
+        return self::formatMysql(new DateTime());
+    }
+
     public static function formatMysql(DateTime $dateTime): string
     {
         return self::format($dateTime, 'Y-m-d H:i:s');
